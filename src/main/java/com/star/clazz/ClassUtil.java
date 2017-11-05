503768c6c816bcc65d43a7ae001b0a130484f756
starhq <50849806@qq.com>
2017-11-05 23:40:11 +0800

    完成ReflectUtil

--- src/main/java/com/star/clazz/ClassUtil.java
+++ src/main/java/com/star/clazz/ClassUtil.java
@@ -124,4 +124,60 @@ public final class ClassUtil {
         final Class<?> clazz = obj.getClass();
         return clazz.getComponentType();
     }
+
+    /**
+     * 获取指定类型分的默认值<br>
+     * <p>
+     * 默认值规则为：
+     * <p>
+     * <pre>
+     *
+     * 1、如果为原始类型，返回0
+     *
+     * 2、非原始类型返回{@code null}
+     *
+     * </pre>
+     *
+     * @param clazz 类
+     * @return 默认值
+     * @since 3.0.8
+     */
+    public static Object getDefaultValue(Class<?> clazz) {
+        if (clazz.isPrimitive()) {
+            if (long.class == clazz) {
+                return 0L;
+            } else if (int.class == clazz) {
+                return 0;
+            } else if (short.class == clazz) {
+                return (short) 0;
+            } else if (char.class == clazz) {
+                return (char) 0;
+            } else if (byte.class == clazz) {
+                return (byte) 0;
+            } else if (double.class == clazz) {
+                return 0D;
+            } else if (float.class == clazz) {
+                return 0f;
+            } else if (boolean.class == clazz) {
+                return false;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 获得默认值列表
+     *
+     * @param classes 值类型
+     * @return 默认值列表
+     * @since 3.0.9
+     */
+    public static Object[] getDefaultValues(Class<?>... classes) {
+        final Object[] values = new Object[classes.length];
+        for (int i = 0; i < classes.length; i++) {
+            values[i] = getDefaultValue(classes[i]);
+        }
+        return values;
+    }
 }
