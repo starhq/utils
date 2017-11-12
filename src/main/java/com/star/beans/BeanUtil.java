a76f3ba2a5552d9dd52a2abbcee3f43601899ce5
starhq <50849806@qq.com>
2017-11-12 21:43:20 +0800

    BeanUtil基本功能有了

--- src/main/java/com/star/beans/BeanUtil.java
+++ src/main/java/com/star/beans/BeanUtil.java
@@ -2,6 +2,7 @@ package com.star.beans;
 
 import com.star.clazz.ClassUtil;
 import com.star.collection.array.ArrayUtil;
+import com.star.collection.map.MapUtil;
 import com.star.exception.ToolException;
 import com.star.reflect.MethodUtil;
 import com.star.string.StringUtil;
@@ -10,7 +11,6 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -46,9 +46,10 @@ public final class BeanUtil {
     };
 
     /**
-     * 常量
+     * class常量
      */
     private static final String CLASS = "class";
+
 
     private BeanUtil() {
     }
@@ -63,6 +64,33 @@ public final class BeanUtil {
         return CACHE.get(clazz);
     }
 
+
+    /**
+     * 获取对象的简单属性
+     *
+     * @param instance 实例
+     * @param name     属性名
+     * @return 对应属性的值
+     */
+    public static <T> Object getSimpleProperty(final T instance, final String name) {
+        final PropertyDescriptor descriptor = getDescriptor(instance, name);
+        final Method method = descriptor.getReadMethod();
+        return MethodUtil.invoke(instance, method);
+    }
+
+    /**
+     * 为对象设置简单属性
+     *
+     * @param instance 实例
+     * @param name     属性名
+     * @param value    值
+     */
+    public static <T> void setSimpleProperty(final T instance, final String name, final Object value) {
+        final PropertyDescriptor descriptor = getDescriptor(instance, name);
+        final Method method = descriptor.getWriteMethod();
+        MethodUtil.invoke(instance, method, value);
+    }
+
     /**
      * 对象转map
      *
@@ -71,23 +99,46 @@ public final class BeanUtil {
      */
     public static <T> Map<String, Object> beanToMap(final T instance) {
         final PropertyDescriptor[] descriptors = getPropertyDescriptors(instance.getClass());
-        final Map<String, Object> maps = new HashMap<>(descriptors.length);
+        final Map<String, Object> maps = MapUtil.newHashMap(descriptors.length);
         for (final PropertyDescriptor descriptor : descriptors) {
             final String name = descriptor.getName();
             if (!CLASS.equals(name)) {
-                try {
-                    final Object value = getSimpleProperty(instance, name, descriptor);
-                    if (!Objects.isNull(value)) {
-                        maps.put(name, value);
-                    }
-                } catch (IllegalArgumentException e) {
-                    throw new ToolException(
-                            StringUtil.format("instance to map failure,the reasone is: {}", e.getMessage()), e);
+                final Object value = getSimpleProperty(instance, name);
+                if (!Objects.isNull(value)) {
+                    maps.put(name, value);
                 }
-
             }
         }
         return maps;
+    }
+
+    /**
+     * 复制Bean对象属性
+     *
+     * @param source           源
+     * @param target           目标
+     * @param ignoreProperties 忽略的属性值
+     */
+    public static void copyProperties(final Object source, final Object target, final String... ignoreProperties) {
+        final PropertyDescriptor[] sourceDescriptors = getPropertyDescriptors(source.getClass());
+        final PropertyDescriptor[] targetDescriptors = getPropertyDescriptors(target.getClass());
+        final int len = sourceDescriptors.length;
+        for (int i = 0; i < len; i++) {
+            final PropertyDescriptor sourceDescriptor = sourceDescriptors[i];
+            final String sourceName = sourceDescriptor.getName();
+            if (CLASS.equals(sourceName) || ArrayUtil.contains(ignoreProperties, sourceName)) {
+                continue;
+            }
+            final PropertyDescriptor targetDescriptor = targetDescriptors[i];
+            final String targetName = targetDescriptor.getName();
+            if (sourceName.equals(targetName)) {
+                final Object value = getSimpleProperty(source, sourceName);
+                if (!Objects.isNull(value)) {
+                    setSimpleProperty(target, targetName, value);
+                }
+            }
+        }
+
     }
 
     /**
@@ -103,102 +154,39 @@ public final class BeanUtil {
         for (final PropertyDescriptor descriptor : descriptors) {
             final String name = descriptor.getName();
             if (!CLASS.equals(name)) {
-                setSimpleProperty(instance, name, maps.get(name), descriptor);
+                setSimpleProperty(instance, name, maps.get(name));
             }
         }
         return instance;
     }
 
     /**
-     * 为对象设置简单属性
+     * 获得实例中指定的propertydescriptor
      *
      * @param instance 实例
      * @param name     属性名
-     * @param value    值
-     * @param desc     PropertyDescriptors实例
+     * @param <T>      泛型
+     * @return 返回propertydescriptor
      */
-    public static <T> void setSimpleProperty(final T instance, final String name, final Object value,
-                                             final PropertyDescriptor desc) {
-        PropertyDescriptor descriptor;
-        if (Objects.isNull(desc)) {
-            try {
-                descriptor = new PropertyDescriptor(name, instance.getClass());
-            } catch (IntrospectionException e) {
-                throw new ToolException(StringUtil.format("filed {} set value {} failure,the reason is: {}", name,
-                        value, e.getMessage()), e);
-            }
-        } else {
-            descriptor = desc;
-        }
-        final Method method = descriptor.getWriteMethod();
-        MethodUtil.invoke(instance, method, value);
-    }
 
-    /**
-     * 获取对象的简单属性
-     *
-     * @param instance 实例
-     * @param name     属性名
-     * @param desc     PropertyDescriptors实例
-     * @return 对应属性的值
-     */
-    public static <T> Object getSimpleProperty(final T instance, final String name, final PropertyDescriptor desc) {
-        PropertyDescriptor descriptor;
-        if (Objects.isNull(desc)) {
+    private static <T> PropertyDescriptor getDescriptor(final T instance, final String name) {
+        PropertyDescriptor descriptor = null;
+        final PropertyDescriptor[] descriptors = getPropertyDescriptors(instance.getClass());
+        for (final PropertyDescriptor desc : descriptors) {
+            if (name.equals(desc.getName())) {
+                descriptor = desc;
+                break;
+            }
+        }
+        if (Objects.isNull(descriptor)) {
             try {
                 descriptor = new PropertyDescriptor(name, instance.getClass());
             } catch (IntrospectionException e) {
                 throw new ToolException(
-                        StringUtil.format("get files {}'s value failure,the reason is: {}", name, e.getMessage()), e);
-            }
-        } else {
-            descriptor = desc;
-        }
-        final Method method = descriptor.getReadMethod();
-        return MethodUtil.invoke(instance, method);
-    }
-
-    /**
-     * 复制Bean对象属性
-     *
-     * @param source           源
-     * @param target           目标
-     * @param ignoreProperties 忽略的属性值
-     */
-    public static void copyProperties(final Object source, final Object target, final String... ignoreProperties) {
-        copyProperties(source, target, null, ignoreProperties);
-    }
-
-    /**
-     * 复制Bean对象属性
-     *
-     * @param source           源
-     * @param target           目标
-     * @param editable         target的父类，只复制target父类中的属性
-     * @param ignoreProperties 忽略的属性值
-     */
-    public static void copyProperties(final Object source, final Object target, final Class<?> editable,
-                                      final String... ignoreProperties) {
-        final Class<?> actualEditable = !Objects.isNull(editable) && editable.isInstance(target) ? editable
-                : target.getClass();
-
-        final PropertyDescriptor[] targetDescriptors = getPropertyDescriptors(actualEditable);
-        final PropertyDescriptor[] sourceDescriptors = getPropertyDescriptors(source.getClass());
-        for (final PropertyDescriptor targetDescriptor : targetDescriptors) {
-            final String name = targetDescriptor.getName();
-            if (CLASS.equals(name) || ArrayUtil.contains(ignoreProperties, name)) {
-                continue;
-            }
-
-            for (final PropertyDescriptor sourceDescriptor : sourceDescriptors) {
-                if (name.equals(sourceDescriptor.getName())) {
-                    final Object value = getSimpleProperty(source, name, sourceDescriptor);
-                    if (!Objects.isNull(value)) {
-                        setSimpleProperty(target, name, value, targetDescriptor);
-                        break;
-                    }
-                }
+                        StringUtil.format("get instance {}'s fileld {}'s propertyDescriptor failure,the reason is: {}", ClassUtil.getClassName
+                                (instance, false), name, e.getMessage()), e);
             }
         }
+        return descriptor;
     }
 }
---
+++ src/test/java/com/star/test/BeanUtilTest.java
@@ -0,0 +1,50 @@
+package com.star.test;
+
+import com.alisoft.nano.bench.Nano;
+import com.star.beans.BeanUtil;
+import org.junit.Test;
+
+public class BeanUtilTest {
+
+    private static int measurements = 100; // 测量次数
+
+    private static int threads = 100; // 线程数
+
+    private static int SerialTimes = 1000; // 每个线程执行序列化次数
+
+
+    @Test
+    public void testCache() {
+        Demo demo = new Demo();
+        demo.setName("hello");
+        Demo demo1 = new Demo();
+        BeanUtil.copyProperties(demo, demo1);
+        System.out.println(demo1.getName());
+
+//        Nano.bench().measurements(measurements).threads(threads).measure("test cache version", () -> {
+//            for (int i = 0; i < SerialTimes; i++) {
+//                BeanUtil.getSimpleProperty(demo, "name");
+//            }
+//        });
+    }
+
+    @Test
+    public void test() {
+        Nano.bench().measurements(measurements).threads(threads).measure("test normal version", () -> {
+            for (int i = 0; i < SerialTimes; i++) {
+            }
+        });
+    }
+
+    class Demo {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
